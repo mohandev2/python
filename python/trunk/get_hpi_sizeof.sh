c0c5e4fa35806a4053d6225a65c8cf6075d946db
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2008-03-04 05:44:26 +0000

    Adjusted script to catch HPI 64bit types

--- python/trunk/get_hpi_sizeof.sh
+++ python/trunk/get_hpi_sizeof.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 # This will scan the SaHpi.h file or equivalent (required argument)
 # looking for SaHpi types and generate a swig sizeof macro for it.
-egrep "SaHpi.*T;" $1 | sed -e 's/^.*\(Sa[A-Za-z0-9]\+T\);.*/%sizeof(\1)/g'
+pass1=`egrep "SaHpi.*T;" $1`
+pass2=`egrep "SaHpi.*T __attribute__\(\(__aligned__\(8\)\)\);" $1`
+
+result1=`echo "$pass1" | sed -e 's/^.*\(Sa[A-Za-z0-9]\+T\);.*/%sizeof(\1)/g'`
+result2=`echo "$pass2" | sed -e 's/^.*\(Sa[A-Za-z0-9]\+T\).*;.*/%sizeof(\1)/g'`
+
+echo "$result2"
+echo "$result1"
