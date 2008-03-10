ab0e8091780b3a4006dc6bf5572d003d1e77759f
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2008-03-10 19:38:39 +0000

    Bumping pyopenhpi to version 1.2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ includes = ['/usr/include/openhpi', '/usr/local/include/openhpi']
 includes += get_glib_cflags()
 
 setup(name='py-openhpi',
-      version='1.1',
+      version='1.2',
       description='Python extension for OpenHPI',
       author='Renier Morales',
       author_email='renier@openhpi.org',
